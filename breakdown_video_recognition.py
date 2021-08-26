a6ebf901e12970a55b3692b2566c7b95b9ecb1d9
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2021-08-26 14:55:37 +0100

    No message

--- breakdown_video_recognition.py
+++ breakdown_video_recognition.py
@@ -1,5 +1,5 @@
 import cv2
-
+from shape_detection import detect_shape
 # Opens the Video file
 cap= cv2.VideoCapture('./20210826_101644.mp4')
 i=0
@@ -7,7 +7,7 @@ while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
         break
-    cv2.imwrite('kang'+str(i)+'.jpg',frame)
+    detect_shape(frame)
     i+=1
 
 cap.release()
--- contour_detection.py
+++ contour_detection.py
@@ -1,7 +1,39 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
-# read the image
-image = cv2.imread("thumbs_up_down.jpg")
+i=1
+while i <= 100:
+    j = 1
+    i = i + 5
+    while j <= 100:
+        j = j + 5
+        # read the image
+        image = cv2.imread("./images/trial4.jpeg")
+        #./images/20210826_114745.jpg
+
+        # convert to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+        # perform edge detection
+        edges = cv2.Canny(gray, i, j)
+
+
+
+        # detect lines in the image using hough lines technique
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 60, np.array([]), 50, 5)
+
+
+
+
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                print(x1, y1, x2, y2)
+                cv2.line(image, (x1, y1), (x2, y2), (20, 220, 20), 3)
+
+        # show the image
+        plt.imshow(image)
+        plt.savefig("./output/thes1 {} thres2 {}".format(i,j))
+
 
 
